0d2212270048a91b9f809ca60397f92557dba6fd
María Agustina Cassi <agustinacassi@Ordenador-portatil-de-Maria.local>
2023-02-06 13:12:57 -0300

    Exercise 2, 06.02.2023

---
+++ Katas/Level-8/challenges/positiveSum.js
@@ -0,0 +1,18 @@
+//1. Description
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+//2. My Solution
+function positiveSum(arr) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++){
+      Math.sign(arr[i]) === 1 ? num += arr[i] : null;
+    }
+    return num;
+}
+
+//3. Refactor with better coding practices
+function positiveSum(arr) {
+    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+}
